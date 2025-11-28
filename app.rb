87babc5eddf698e0d64e72a45aba5d97cb520f81
anthonydhavid <anthonydhavidds@gmail.com>
2025-11-28 16:48:08 +0000

    :art: style: padroniza mensagem de erro com emoji

--- app.rb
+++ app.rb
@@ -1,4 +1,4 @@
-# Carrega as bibliotecas do Bundlergit add app.rb
+# Carrega as bibliotecas do Bundler
 "#!/usr/bin/env ruby"
 require 'bundler/setup'
 require 'kramdown'
@@ -33,6 +33,6 @@ if File.exist?(caminho_arquivo)
   
   puts pastel.green.bold("✔ Sucesso! Arquivo gerado: #{nome_saida}")
 else
-  puts pastel.red.bold("Erro: Arquivo '#{caminho_arquivo}' não encontrado.")
+  puts pastel.red.bold("❌ Erro Crítico: O arquivo solicitado não foi localizado.")
   exit(1)
 end
